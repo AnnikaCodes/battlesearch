703fc24140133a4960c9708261d16ba87eff6809
Annika <annika0uwu@gmail.com>
2021-04-16 17:59:36 -0700

    Add the code

---
+++ src/main.rs
@@ -0,0 +1,110 @@
+/// Battlesearch code for Pokémon Showdown battle logs
+
+mod search;
+
+use std::{path::PathBuf, sync::mpsc, thread};
+use search::{BattleSearchError, BattleSearcher, ToSend};
+use structopt::StructOpt;
+
+const PIKKR_TRAINING_ROUNDS: usize = 2;
+
+fn get_filename(file: &PathBuf) -> Result<String, BattleSearchError> {
+    match file.file_name() {
+        Some(os_str) => match os_str.to_str() {
+            Some(s) => Ok(String::from(s)),
+            None => return Err(BattleSearchError::Path(format!("Couldn't get filename of {:?}", file))),
+        },
+        None => return Err(BattleSearchError::Path(format!("Couldn't get filename of {:?}", file))),
+    }
+}
+
+fn handle_dir(directory: &PathBuf, threads: &Vec<mpsc::Sender<ToSend>>) -> Result<(), BattleSearchError> {
+    let mut current_sender_idx = 0;
+    let num_threads = threads.len();
+
+    let contents = directory.read_dir()?;
+    let date = get_filename(directory)?;
+    for entry in contents {
+        if let Ok(file) = entry {
+            if file.file_type()?.is_dir() {
+                handle_dir(&file.path(), &threads)?;
+            } else {
+                threads.get(current_sender_idx).unwrap().send(ToSend::File(file.path(), date.clone())).unwrap_or_else(|e| {
+                    println!("{:?}", e);
+                });
+                current_sender_idx = (current_sender_idx + 1) % num_threads;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case", author = "Annika L.", about = "Searches Pokémon Showdown battle logs")]
+struct Options {
+    #[structopt(short = "w", long = "wins-only", help = "Only display games where the searched user wins")]
+    wins_only: bool,
+
+    #[structopt(short = "f", long = "forfeits-only", help = "Only display games that end with one player forfeiting")]
+    forfeits_only: bool,
+
+    #[structopt(short = "j", long = "threads", help = "The number of threads to spawn", default_value = "2")]
+    threads: u32,
+
+    #[structopt(help = "The username whose battles will be displayed")]
+    username: String,
+
+    #[structopt(help = "The directories to search for battle logs in. Searches recursively.", required(true))]
+    #[structopt(parse(from_os_str))]
+    directories: Vec<PathBuf>,
+}
+
+fn main() -> Result<(), BattleSearchError> {
+    let options = Options::from_args();
+    let mut senders = vec![];
+    let mut join_handles = vec![];
+    for _ in 1..=options.threads {
+        let (sender, receiver) = mpsc::channel();
+        let username = options.username.clone();
+        let wins_only = options.wins_only;
+        let forfeits_only = options.forfeits_only;
+        join_handles.push(thread::spawn(move || {
+            let mut searcher = BattleSearcher::new(
+                &username,
+                PIKKR_TRAINING_ROUNDS,
+                wins_only,
+                forfeits_only
+            );
+            loop {
+                match receiver.recv() {
+                    Ok(data) => match data {
+                        ToSend::File(path, date) => if let Err(e) = searcher.check_log(&path, &date) {
+                            eprintln!("Error parsing {:?}: {:?}", path, e);
+                        },
+                        ToSend::Done => return,
+                    },
+                    Err(e) => {
+                        eprintln!("{:?}", e);
+                        return;
+                    } 
+                }
+            }
+        }));
+        senders.push(sender);
+    }
+    
+    for directory in &(options.directories) {
+        handle_dir(directory, &senders)?;
+    }
+
+    for sender in senders {
+        sender.send(ToSend::Done)?;
+    }
+    
+    for handle in join_handles {
+        handle.join()?;
+    }
+
+    Ok(())
+}
---
+++ src/search.rs
@@ -0,0 +1,146 @@
+/// Battlesearch code for Pokémon Showdown battle logs
+
+use std::{any::Any, fs, path::PathBuf, sync::mpsc::Sender};
+use regex::Regex;
+use lazy_static::*;
+
+#[derive(Debug)]
+pub enum BattleSearchError {
+    FaultyJSON(String),
+    Path(String),
+    IO(std::io::Error),
+    Thread(std::sync::mpsc::SendError<ToSend>),
+    Join(Box<dyn Any + Send>),
+}
+
+pub enum ToSend {
+    File(PathBuf, String),
+    Done,
+}
+
+impl From<std::io::Error> for BattleSearchError {
+    fn from(err: std::io::Error) -> Self {
+        BattleSearchError::IO(err)
+    }
+}
+impl From<std::sync::mpsc::SendError<ToSend>> for BattleSearchError {
+    fn from(err: std::sync::mpsc::SendError<ToSend>) -> Self {
+        BattleSearchError::Thread(err)
+    }
+}
+impl From<Box<dyn Any + Send>> for BattleSearchError {
+    fn from(err: Box<dyn Any + Send>) -> Self {
+        BattleSearchError::Join(err)
+    }
+}
+
+lazy_static! {
+    static ref ID_REGEX: Regex = Regex::new(r"[^A-Za-z0-9]").unwrap();
+}
+
+// Taken from https://github.com/AnnikaCodes/anonbattle/blob/main/src/anonymizer.rs#L36
+// Perhaps I should share code somehow in the future; perhaps with a battle-tools library crate?
+fn str_to_id(str: &str) -> String {
+    (*ID_REGEX.replace_all(str, "")).to_lowercase()
+}
+
+fn bytes_to_id(bytes: &Option<&[u8]>) -> Option<String> {
+    match bytes {
+        Some(b) => Some(str_to_id(&String::from_utf8_lossy(b))),
+        None => None,
+    }
+}
+
+pub struct BattleSearcher<'a> {
+    user_id: String,
+    json_parser: pikkr_annika::Pikkr<'a>,
+    wins_only: bool,
+    forfeits_only: bool,
+}
+
+impl<'a> BattleSearcher<'a> {
+    pub fn new(username: &str, pikkr_training_rounds: usize, wins_only: bool, forfeits_only: bool) -> Self {
+        let json_parser = pikkr_annika::Pikkr::new(&vec![
+            "$.p1".as_bytes(), // p1 name - idx 0
+            "$.p2".as_bytes(), // p2 name - idx 1
+            "$.winner".as_bytes(), // winner - idx 2
+            "$.endType".as_bytes(), // end type - idx 3
+        ], pikkr_training_rounds).unwrap();
+
+        Self { user_id: str_to_id(username), json_parser, wins_only, forfeits_only }
+    }
+
+    /// json is in the form [p1name, p2name, winner, endType]
+    pub fn check_log(&mut self, path: &PathBuf, date: &str) -> Result<(), BattleSearchError> {
+        let data = fs::read(path)?;
+        let json = self.json_parser.parse(&data).unwrap();
+
+        if json.len() != 4 {
+            // should never happen
+            return Err(BattleSearchError::FaultyJSON(format!(
+                "BattleSearcher::check_log(): found {} elements in parsed JSON (expected 4)", json.len()
+            )));
+        }
+
+        // parse players
+        let p1id = match bytes_to_id(json.get(0).unwrap()) {
+            Some(a) => a,
+            None => return Err(BattleSearchError::FaultyJSON(format!("No p1 value"))),
+        };
+        let p2id = match bytes_to_id(json.get(1).unwrap()) {
+            Some(a) => a,
+            None => return Err(BattleSearchError::FaultyJSON(format!("No p2 value"))),
+        };
+        let p1_is_searched_user = p1id == self.user_id;
+        let p2_is_searched_user = p2id == self.user_id;
+        if !p1_is_searched_user && !p2_is_searched_user {
+            // Searched user is not a player in the battle.
+            return Ok(());
+        }
+
+        // parse winner
+        let winner_id = bytes_to_id(json.get(0).unwrap());
+        let searched_user_won = match winner_id {
+            Some(ref winner) => winner == &self.user_id,
+            None => false,
+        };
+        if !searched_user_won && self.wins_only {
+            return Ok(());
+        }
+
+        // parse endType
+        let is_forfeit = match json.get(3).unwrap() {
+            Some(bytes) => String::from_utf8_lossy(bytes) == "forfeit",
+            None => false,
+        };
+        if !is_forfeit && self.forfeits_only {
+            return Ok(());
+        }
+
+        // formatting
+        let win_type_str = if is_forfeit {
+            "by forfeit"
+        } else {
+            "normally"
+        };
+        let win_str = match winner_id {
+            Some(ref winner) => format!("{} won {}", winner, win_type_str),
+            None => String::from("there was no winner"),
+        };
+
+        let room = match path.file_name() {
+            Some(os_str) => String::from(os_str.to_str().unwrap_or("unknown file")),
+            None => String::from("unknown file")
+        }.replace(".log.json", "");
+
+        println!("({}) <<{}>> {} vs. {} ({})", date, room, p1id, p2id, win_str);
+
+        Ok(())
+    }
+
+    /// Recursively checks a directory
+    pub fn check_directory(&mut self, directory: PathBuf) -> Result<(), BattleSearchError> {
+
+        Ok(())
+    }
+}
